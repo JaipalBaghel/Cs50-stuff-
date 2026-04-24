21bddaadb3afc71579efc2c685d015cfd3000c0e
JaipalBaghel <jaipalbaghel24@gmail.com>
2026-04-24 16:44:31 +0530

    Add files via upload

---
+++ agree.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    char c = get_char("Do you agree? ");
+
+    if (c == 'y'  || c == 'Y')
+    {
+        printf("agreed\n");
+    }
+    else
+    {
+        printf("not agreed\n");
+    }
+
+}
+
+
+
+
+
+
+
+
